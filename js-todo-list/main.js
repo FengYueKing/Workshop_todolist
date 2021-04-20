857f9eea5cab2d019cc7aba8b150788e26b453ae
xiao-tang <10761101@gm.nfu.edu.tw>
2021-04-20 20:27:17 +0800

    answer_two

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,10 +1,19 @@
 // TO DO
 document.addEventListener("DOMContentLoaded", function(){
 
-  // 第一題
-  document.querySelectorAll('li').forEach(function(list) {
-    list.addEventListener('click', function(e){
-      e.target.classList.toggle('checked');
+  // // 第一題
+  // document.querySelectorAll('li').forEach(function(list) {
+  //   list.addEventListener('click', function(e){
+  //     e.target.classList.toggle('checked');
+  //   })
+  // })
+
+    // 第二題
+    document.querySelectorAll('.close').forEach(function(list) {
+      list.addEventListener('click', function(e){
+        e.target.parentNode.remove();
+      })
     })
-  })
+  
+    
 })
